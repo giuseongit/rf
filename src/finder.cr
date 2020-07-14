1c6222a2371f75fbdf9acae31a221a0da6cb8548
Giuseppe Pagano <giuseppe.pagano.p@gmail.com>
2020-07-14 17:18:06 +0200

    Add capability to decet subrepositories

--- src/finder.cr
+++ src/finder.cr
@@ -3,10 +3,10 @@ require "colorize"
 
 module Rf
   class Finder
-    def initialize(@enabled_vscs : SupportedVSC)
+    def initialize(@enabled_vscs : SupportedVSC, @max_subrepo_depth : Int32)
     end
 
-    def walk_dirs(root : Dir) : Array(Index | Int32)
+    def walk_dirs(root : Dir, depth : Int32 = 1) : Array(Index | Int32)
       sublogger = Loggers.for("walk")
       walked_dirs = 0
       sublogger.debug { "visiting #{root}" }
@@ -14,15 +14,19 @@ module Rf
       root.each_subdir do |subdir|
         sublogger.debug { "found #{subdir}" }
         walked_dirs += 1
+        repo_depth = depth
         if (@enabled_vscs.permits_git? && subdir.is_git_repo?) ||
            (@enabled_vscs.permits_svn? && subdir.is_svn_repo?)
           sublogger.info { "adding #{subdir}" }
           repos << subdir
-        else
-          res = walk_dirs(subdir)
-          repos.concat res[0].as(Array)
-          walked_dirs += res[1].as(Int)
+          if depth >= @max_subrepo_depth
+            next
+          end
+          repo_depth += 1
         end
+        res = walk_dirs(subdir, repo_depth)
+        repos.concat res[0].as(Array)
+        walked_dirs += res[1].as(Int)
       end
       root.close
       return [repos, walked_dirs]
